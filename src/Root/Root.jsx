9db094d0be478f673bdbc3c3a4cc9df0b68c3911
mdimranictiu <imranictiu11@gmail.com>
2025-03-17 12:16:45 +0600

    QuickCart Project Start

---
+++ src/Root/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+           <h2 className=' py-10 text-center text-5xl'>QuickCart</h2> 
+           
+        </div>
+    );
+};
+
+export default Root;
